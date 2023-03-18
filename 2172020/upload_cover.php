15269e14317e2e6d6d9a7b25caf671fa406c63c3
NicholasRudy2172020 <91111940+NicholasRudy2172020@users.noreply.github.com>
2023-03-18 20:28:27 +0700

    Update upload_cover.php

--- 2172020/upload_cover.php
+++ 2172020/upload_cover.php
@@ -1,1 +1,40 @@
+<?php
+$editedISBN = filter_input(INPUT_GET, 'isbn');
+if(isset($editedISBN)){
+    $book = fetchOneBook($editedISBN);
+    $bookgenre = fetchOneBookName($editedISBN);
+}
+$uploadRequest = filter_input(INPUT_POST, 'btnUpload');
+if (isset($uploadRequest)) {
+    $fileExtension = pathinfo($_FILES['imageFile']['name'], PATHINFO_EXTENSION);
+    $fileName = $book['isbn'];
+    $targetDirectory = 'uploads/';
+    $file = $fileName . '.' . $fileExtension;
+    $pathToUpload = $targetDirectory . $file;
+    if ($_FILES['imageFile']['size'] > 1024*2048){
+        echo '<div>File size exceed 2MB. Please choose another file.</div>';
+    } else{
+        move_uploaded_file($_FILES['imageFile']['tmp_name'], $pathToUpload);
+        $book = "UPDATE book SET Cover='".$file."' WHERE Cover IS NULL";
+    }
+}
 
+    echo"<div>Current Cover</div>";
+    if (is_null($book['Cover'])) {
+        $url = '<img class="img-tbl" src="uploads/ComingSoon.PNG"></img>';
+      }
+      else {
+        $url = '<img class="img-tbl" src="uploads/'. $book['Cover'].'"></img>';
+      }
+    echo $url;
+    ?>
+    <br>
+<div class="container">
+    <form method="post" enctype="multipart/form-data">
+        <input type="file" name="imageFile" accept="image/*" class="form-control">
+        <br>
+        <div>
+            <input type="submit" name="btnUpload" value="Upload Image" class="btn btn-primary">
+        </div>
+    </form>
+</div>
