246bcdf5a75e0766222cc46dbd3d51d9f4cf4cba
NicholasRudy2172020 <91111940+NicholasRudy2172020@users.noreply.github.com>
2023-03-18 20:26:48 +0700

    Update command_script.js

--- 2172020/command_script.js
+++ 2172020/command_script.js
@@ -1,1 +1,33 @@
 
+//Genre
+function updateValue(id) {
+    window.location = "index.php?menu=upd&genreID=" + id;
+    return false;
+}
+
+function deleteValue(id) {
+    let confirmation = window.confirm("Are you sure you want to delete this data?")
+    if(confirmation) {
+        window.location = "index.php?menu=genre&cmd=del&genreID=" + id;
+        return false;
+    }
+}
+
+
+//Book
+function updateBook(isbn) {
+    window.location = "index.php?menu=updBook&isbn=" + isbn;
+    return false;
+}
+
+function bookCov(isbn){
+    window.location = "index.php?menu=updBookCov&isbn=" + isbn;
+    return false;
+}
+function deleteBook(isbn) {
+    let confirmation = window.confirm("Are you sure you want to delete this data?")
+    if(confirmation) {
+        window.location = "index.php?menu=book&cmd=del&isbn=" + isbn;
+        return false;
+    }
+}
